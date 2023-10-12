698c7dfcb3b030a6bbff27d14db54a3631eecb78
Smuzzies <110495122+Smuzzies@users.noreply.github.com>
2023-10-12 16:48:27 -0500

    Update chatbox_overlay.py

--- chatbox_overlay.py
+++ chatbox_overlay.py
@@ -15,7 +15,7 @@ class ChatboxOverlay:
                 "text": ("STRING", {"multiline": True, "default": "Hello"}),
                 "textbox_width": ("INT", {"default": 200, "min": 1}),  # Define textbox width in pixels
                 "textbox_height": ("INT", {"default": 100, "min": 1}),  # Define textbox height in pixels
-                "font_size": ("INT", {"default": 16, "min": 1, "max": 256, "step": 1}),
+                "max_font_size": ("INT", {"default": 256, "min": 1, "max": 256, "step": 1}),  # Maximum font size to try
                 "font": ("STRING", {"default": "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf"}),  # Use DejaVu Sans as the default font
                 "alignment": (cls._alignments, {"default": "center"}),  # ["left", "right", "center"]
                 "color": ("STRING", {"default": "#000000"}),  # Accept hex value for color
@@ -28,7 +28,7 @@ class ChatboxOverlay:
     FUNCTION = "draw_text_on_image"
     CATEGORY = "image/text"
 
-    def draw_text_on_image(self, image, text, textbox_width, textbox_height, font_size, font, alignment, color, start_x, start_y):
+    def draw_text_on_image(self, image, text, textbox_width, textbox_height, max_font_size, font, alignment, color, start_x, start_y):
         # Convert tensor to numpy array and then to PIL Image
         image_tensor = image
         image_np = image_tensor.cpu().numpy()  # Change from CxHxW to HxWxC for Pillow
@@ -37,54 +37,69 @@ class ChatboxOverlay:
         # Convert hex color to RGB tuple
         color_rgb = tuple(int(color.lstrip("#")[i:i+2], 16) for i in (0, 2, 4))
 
-        # Load font
-        loaded_font = ImageFont.truetype(font, font_size)
+        # Start with the maximum font size
+        font_size = max_font_size
 
-        # Prepare to draw on image
-        draw = ImageDraw.Draw(image)
+        while font_size >= 1:
+            # Load font
+            loaded_font = ImageFont.truetype(font, font_size)
 
-        # Calculate text dimensions
-        text_width, text_height = loaded_font.getsize(text)
+            # Prepare to draw on image
+            draw = ImageDraw.Draw(image)
 
-        # Calculate the number of lines needed for word wrapping
-        lines = []
-        words = text.split()
-        current_line = ""
-        for word in words:
-            if current_line == "":
-                current_line = word
-            elif draw.textsize(current_line + " " + word, font=loaded_font)[0] <= textbox_width:
-                current_line += " " + word
-            else:
-                lines.append(current_line)
-                current_line = word
-        if current_line:
-            lines.append(current_line)
+            # Calculate text dimensions of the wrapped text
+            wrapped_text = self.wrap_text(text, loaded_font, textbox_width)
+            text_width, text_height = draw.textsize(wrapped_text, font=loaded_font)
 
-        # Calculate total text height for vertical centering
-        total_text_height = len(lines) * text_height
+            # Check if text fits within the textbox dimensions
+            if text_width <= textbox_width and text_height <= textbox_height:
+                # Calculate total text height for vertical centering
+                lines = wrapped_text.split('\n')
+                total_text_height = len(lines) * loaded_font.getsize("M")[1]  # Use font ascent for line height
 
-        # Calculate y-coordinate for vertical centering
-        y = start_y + ((textbox_height - total_text_height) // 2)
+                # Calculate y-coordinate for vertical centering, considering font ascent
+                y = start_y + ((textbox_height - total_text_height) // 2) + loaded_font.getsize("M")[1] // 2
 
-        # Draw each line of text
-        for line in lines:
-            # Calculate x-coordinate for horizontal centering
-            x = start_x  # Start from the specified X-coordinate
-            if alignment == "center":
-                x += (textbox_width - draw.textsize(line, font=loaded_font)[0]) // 2
-            elif alignment == "right":
-                x += textbox_width - draw.textsize(line, font=loaded_font)[0]
+                # Draw the wrapped text on the image
+                x = start_x  # Start from the specified X-coordinate
+                for line in lines:
+                    line_width, line_height = draw.textsize(line, font=loaded_font)
+                    if alignment == "center":
+                        x = start_x + (textbox_width - line_width) // 2
+                    elif alignment == "right":
+                        x = start_x + (textbox_width - line_width)
+                    draw.text((x, y), line, fill=color_rgb, font=loaded_font)
+                    y += loaded_font.getsize("M")[1]  # Use font ascent for line height
 
-            # Draw text on the image
-            draw.text((x, y), line, fill=color_rgb, font=loaded_font)
-            y += text_height
+                # Convert back to Tensor if needed
+                image_tensor_out = torch.tensor(np.array(image).astype(np.float32) / 255.0)  # Convert back to CxHxW
+                image_tensor_out = torch.unsqueeze(image_tensor_out, 0)
 
-        # Convert back to Tensor if needed
+                return (image_tensor_out,)
+
+            # Reduce font size and try again
+            font_size -= 1
+
+        # If the text cannot fit even at the smallest font size, return the original image
         image_tensor_out = torch.tensor(np.array(image).astype(np.float32) / 255.0)  # Convert back to CxHxW
         image_tensor_out = torch.unsqueeze(image_tensor_out, 0)
-
         return (image_tensor_out,)
+
+    def wrap_text(self, text, font, max_width):
+        lines = []
+        for line in text.split('\n'):
+            words = line.split()
+            wrapped_line = words[0]
+            for word in words[1:]:
+                test_line = wrapped_line + ' ' + word
+                test_size = font.getsize(test_line)
+                if test_size[0] <= max_width:
+                    wrapped_line = test_line
+                else:
+                    lines.append(wrapped_line)
+                    wrapped_line = word
+            lines.append(wrapped_line)
+        return '\n'.join(lines)
 
 NODE_CLASS_MAPPINGS = {
     "Chatbox Overlay": ChatboxOverlay,
